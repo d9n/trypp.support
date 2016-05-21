089c7a18fcdbfcd3d5a9c5b8b645911af2632c9b
David Herman <d9n.coding@gmail.com>
2016-05-21 07:43:22 -0700

    Clarify BitUtils assert message

--- src/main/code/trypp/support/math/BitUtils.kt
+++ src/main/code/trypp/support/math/BitUtils.kt
@@ -2,7 +2,7 @@ package trypp.support.math
 
 object BitUtils {
     fun requireSingleBit(value: Int) {
-        assert(hasSingleBit(value), { "Passed in value should only have a single bit set" })
+        assert(hasSingleBit(value), { "Passed in value should have a single bit set: $value -> ${Integer.toBinaryString(value)}" })
     }
 
     /**
@@ -13,6 +13,9 @@ object BitUtils {
         return value != 0 && value and value - 1 == 0
     }
 
+    /**
+     * Given a bit value, e.g. 10000, return the index of the bit, e.g. 5
+     */
     fun getBitIndex(value: Int): Int {
         requireSingleBit(value)
         for (i in 0..31) {
